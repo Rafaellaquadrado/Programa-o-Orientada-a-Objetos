e3243a231051bc158e01783240fa68749b44c757
LARA RAFAELLA QUADRADO <147094552+Rafaellaquadrado@users.noreply.github.com>
2024-09-09 09:16:25 -0300

    Add files via upload

---
+++ Tempo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Tempo
+{
+  Public static void main(string[] args)
+  {
+   int tempo, dia, hora, min, seg;
+
+   Scanner entrada = new Scanner(System.in);
+
+   System.out.println("Entre com o tempo em segundos:");
+   tempo = entrada.nextInt();
+
+  seg = tempo%60;
+  min = (tempo/60)%60;
+  hora = ((tempo/60)/60)%24;
+  dia = ((tempo/60)/60)/24;
+  }
+}
