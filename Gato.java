11a7e16929a1ee2b27dded6155f8d5dd7201b2fc
LARA RAFAELLA QUADRADO <147094552+Rafaellaquadrado@users.noreply.github.com>
2024-09-09 10:11:09 -0300

    Update Gato.java

--- Gato.java
+++ Gato.java
@@ -4,7 +4,21 @@ public class Gato
   public void miar(String nome)
   {
    System.out.println(nome + "Miou!");
-
   }
+  public void lamber
+  {
+   System.out.println(nome + "lambeu!");
+  }
+
+   public void setNome(String nome)
+   {
+    this.nome = nome;
+   }
+  
+   public String getNome()
+   {
+   return this.nome;
+  }
+
 
 }
