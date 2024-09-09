ba637db6cd34ee6512ebee048e5e12ef3309363a
LARA RAFAELLA QUADRADO <147094552+Rafaellaquadrado@users.noreply.github.com>
2024-09-09 10:27:48 -0300

    Update GatoTest.java

--- GatoTest.java
+++ GatoTest.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class GatoTest
 {
@@ -9,12 +10,14 @@ public class GatoTest
    System.out.println("Qual nome do seu gato:");
    String nome = entrada.nextline();
 
-   Gato gatoDeBotas = new Gato();
-   gatoDeBotas.setnome = (texto);
+   Gato gatoDeBotas = new Gato("jose", "07/09/2010", 0.5);
+   gatoDeBotas.setNome = (texto);
    System.out.printf("O nome do seu gato é: %s\n", gatoDeBotas.getNome); 
    gatoDeBotas.miar();
    gatoDeBotas.lamber();
+
+   //System.out.println("Qual o nome do seu gato:");
+   //String texto = entrada.nextLine();
+
+   String texto = JOptionPane.inputMessageDialog(null, "Qual o nome do seu gato:");
   }
-
-
-}
