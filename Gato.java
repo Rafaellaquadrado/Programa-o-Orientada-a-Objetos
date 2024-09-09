e149e43de878a5d7f0668893288429316562245a
LARA RAFAELLA QUADRADO <147094552+Rafaellaquadrado@users.noreply.github.com>
2024-09-09 10:38:04 -0300

    Update Gato.java

--- Gato.java
+++ Gato.java
@@ -1,13 +1,28 @@
+import javax.swing.JOptionPane;
 public class Gato
 {
+  private String nome;
+  private String dataNasc;
+  private String peso;
+
+  public Gato(String nome, String dataNasc, double peso)
+  {
+    this.nome = nome;
+    this.dataNasc = dataNasc;
+    this.peso = peso;
+  }
+
 
   public void miar(String nome)
   {
-   System.out.println(nome + "Miou!");
+   //System.out.println(this.nome + "Miou!");
+   JOptionPane.showMessageDialog(null, this.nome + "Miou!");
   }
+
+
   public void lamber
   {
-   System.out.println(nome + "lambeu!");
+   System.out.println(this.nome + "lambeu!");
   }
 
    public void setNome(String nome)
