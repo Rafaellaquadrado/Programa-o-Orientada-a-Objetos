6ffbe09bf5816de105795fe612c99be30f9c49b9
LARA RAFAELLA QUADRADO <147094552+Rafaellaquadrado@users.noreply.github.com>
2024-09-16 10:26:22 -0300

    Add files via upload

---
+++ Cosseno.java
@@ -0,0 +1,18 @@
+public class Cosseno
+{
+
+
+   public static void main(String[] args)
+   {
+     int num, fatorial=1;
+     
+     for(int count=num; count>=1; count--)
+     {
+        fatorial = fatorial*count;
+        
+     }
+    
+     System.out.printf("Fatorial de %d é %d, num, fatorial");
+   }
+
+}
